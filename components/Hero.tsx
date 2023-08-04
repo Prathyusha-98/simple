cfd3b5ef6d09c7b33e4f11d5cc553bb6c59797f1
Prathyusha <prathyusha@simplemail.ai>
2023-08-04 19:52:47 +0530

    responsive and uni part

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import Asset from '@/assets/Asset.png'
@@ -9,6 +10,7 @@ import blue from '@/assets/blue.png'
 import violet from '@/assets/violet.png'
 import mobile from '@/assets/mobile.png'
 import hand from '@/assets/hand.png'
+import Uni from './Uni'
 const Hero =() =>{
 
     const sections = [
@@ -19,17 +21,17 @@ const Hero =() =>{
       ];
     return(
         <>
-        <Header/>
+        <div className='lg:block sm:hidden xs:hidden'><Header/></div>
     <div className='relative'>
-      <Image className='w-full 1xl:h-[960px] xl:h-[563px]' src={Asset} alt='' />
+      <Image className='w-full 1xl:h-[960px] xl:h-[563px] sm:h-[600px] xs:h-[600px] sm:w-full' src={Asset} alt='' />
       <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center'>
         <div className='flex justify-center items-center'>
-          <p className='w-[737px] text-[#fff] text-center font-montserrat text-[45px] font-extrabold leading-normal '>Empowering You to <span className='text-transparent bg-clip-text bg-gradient-to-b to-[#3A07E1] from-[#A010E0]' >Excel</span> and Live <span className='text-transparent bg-clip-text bg-gradient-to-b to-[#3A07E1] from-[#A010E0]'>Better</span> Lives</p>
+          <p className='1xl:w-[737px] sm:w-[443px] sm:h-[114px] xs:w-[283px] xs:h-[114px] text-[#fff] text-center font-montserrat 1xl:text-[45px] sm:text-[30px] xs:text-[25px] font-extrabold leading-normal '>Empowering You to <span className='text-transparent bg-clip-text bg-gradient-to-b to-[#3A07E1] from-[#A010E0]' >Excel</span> and Live <span className='text-transparent bg-clip-text bg-gradient-to-b to-[#3A07E1] from-[#A010E0]'>Better</span> Lives</p>
         </div>
-        <div className='flex justify-center items-center mt-[36px]'>
-          <p className='w-[737px] text-[#fff] text-center font-poppins text-base font-light leading-[28px] '>At Simple AI, we are on a mission to develop AI-driven applications that are user-friendly, accessible to all, and ultimately empower individuals to perform at their best and live better lives.</p>
+        <div className='flex justify-center items-center 1xl:mt-[36px] xs:mt-6 '>
+          <p className='1xl:w-[737px] sm:w-[376px] sm:h-[110px] xs:w-[276px] xs:h-[110px] text-[#fff] text-center font-poppins 1xl:text-base xs:text-[12px] font-light leading-[28px] '>At Simple AI, we are on a mission to develop AI-driven applications that are user-friendly, accessible to all, and ultimately empower individuals to perform at their best and live better lives.</p>
         </div>
-        <div className='flex justify-center items-center mt-[69px] 1xl:mb-[330px] '>
+        <div className='flex justify-center items-center 1xl:mt-[69px] 1xl:mb-[330px] xs:mt-12'>
         <div className='w-[191px] h-[50px] text-center bg-gradient-to-r from-[#4EA0FB] via-[#9354F7] to-[#D70BF4] rounded-[186px] flex justify-center items-center'>
             <p className='text-white text-center font-poppins text-base font-bold leading-[19px]' >Get started</p>
             <BsArrowRight className='fill-white text-[24px] font-bold ml-4' />
@@ -38,26 +40,37 @@ const Hero =() =>{
 
       </div>
     </div>
-    <div className='bg-[#0D0037] h-[472px] mt-[108px]'>
-        <div className='flex justify-center items-center '><p className='w-[828px] text-white text-center font-montserrat text-[50px] font-semibold leading-normal'>Unleashing the Power of AI, Simplified</p></div>
-        <div className=' flex justify-center items-center mt-6 '><p className='w-[902px] text-[rgba(255,255,255,0.8)] text-center font-poppins text-sm font-medium leading-normal'>Imagine a world where technology seamlessly integrates into your daily life, making complex tasks effortless and enhancing your capabilities. That’s the vision we strive to bring to reality. Our vision is to be a world leader in providing AI-driven applications that empower individuals to reach their full potential and improve the quality of their lives. We strive to make AI accessible to everyone and create user-friendly solutions that simplify complex tasks.</p></div>
+    <div  className='bg-[#0D0037] h-[472px] xs:h-[420px] sm:h-[420px] mt-[108px]'>
+        <div className='flex justify-center items-center '><p className='1xl:w-[828px] sm:w-[486px] xs:w-[286px] text-white text-center font-montserrat 1xl:text-[50px] sm:text-[30px] xs:text-[20px] font-semibold leading-normal'>Unleashing the Power of AI, Simplified</p></div>
+        <div className=' flex justify-center items-center mt-6 '>
+          <p className='lg:block sm:hidden xs:hidden 1xl:w-[902px] sm:w-[492px] xs:w-[292px] text-[rgba(255,255,255,0.8)] text-center font-poppins 1xl:text-sm sm:text-[10px] xs:text-[10px] font-medium leading-normal'>
+          Imagine a world where technology seamlessly integrates into your daily life, making complex tasks effortless and enhancing your capabilities. That’s the vision we strive to bring to reality. Our vision is to be a world leader in providing AI-driven applications that empower individuals to reach their full potential and improve the quality of their lives. We strive to make AI accessible to everyone and create user-friendly solutions that simplify complex tasks.
+          </p>
+        </div>
+
+
+
+        <div className=' flex justify-center items-center mt-6 '>
+          <p className='lg:hidden sm:block xs:block 1xl:w-[902px] sm:w-[492px] xs:w-[292px] text-[rgba(255,255,255,0.8)] text-center font-poppins 1xl:text-sm sm:text-[10px] xs:text-[10px] font-medium leading-normal'>
+          Imagine a world where technology seamlessly integrates into your daily life, making complex tasks effortless and enhancing your capabilities. That’s the vision we strive to bring to reality. <div className='mt-6' />Our vision is to be a world leader in providing AI-driven applications that empower individuals to reach their full potential and improve the quality of their lives. We strive to make AI accessible to everyone and create user-friendly solutions that simplify complex tasks.
+          </p>
+        </div>
         <div className='mt-12'>
             <Link href='/about' className='flex justify-center items-center'>
-                <button className='flex justify-center items-center bg-clip-text bg-gradient-to-r from-[#4EA0FB] via-[#9354F7] to-[#D70BF4] text-transparent text-center font-montserrat text-base font-medium leading-normal mr-3'>
+                <button className='flex justify-center items-center bg-clip-text bg-gradient-to-r from-[#4EA0FB] via-[#9354F7] to-[#D70BF4] text-transparent text-center font-montserrat 1xl:text-base xs:text-[12px] sm:text-[12px] font-medium leading-normal mr-3'>
                 <span className='underline mr-3 '>Learn More About Us</span>
-                <Image src={arrow} alt='' />
+                <Image className='xs:w-[12px]' src={arrow} alt='' />
                 </button>
             </Link>
         </div>
     </div>
-    <div className='relative'>
-      <Image className='w-full 1xl:h-[772px] xl:h-[772px]' src={blue} alt='' />
-      <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-    <div className='relative'>
+
+
+    <div className=' lg:block sm:hidden xs:hidden'><Uni/></div>
+
+
+
+    <div className='relative lg:block sm:hidden xs:hidden'>
       <Image className='w-full 1xl:h-[499px] xl:h-[499px]' src={violet} alt='' />
       <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center'>
         <div className='flex justify-center items-center'>
@@ -79,7 +92,7 @@ const Hero =() =>{
       </div>
     </div>
 
-    <div className='relative'>
+    <div className='relative lg:block sm:hidden xs:hidden'>
       <Image className='w-full 1xl:h-[963px] xl:h-[963  px]' src={hand} alt='' />
       <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center'>
         <div className='flex justify-center items-center'>
--- components/Uni.tsx
+++ components/Uni.tsx
@@ -1,46 +1,112 @@
 import { useState } from 'react'
 import Image from 'next/image'
+import learn from '@/assets/learn.png'
+import blue from '@/assets/blue.png'
+import arrow from '@/assets/arrow.png'
+import logo from '@/assets/logo.png'
+import Link from 'next/link'
 export default function Uni() {
   const [selected, setSelected] = useState('write')
 
   const writeContent = (
     <>
-      <p>This is the write content.</p>
-      <Image src='/write.png' alt='Write' />
+    <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center'>
+      <div className='w-[399px] h-[364px]  '>
+      <div ><p className='text-white font-poppins text-base font-medium leading-normal mb-[26px]'>Simple Uni</p></div>
+      <div ><p className='text-white font-montserrat text-2xl font-bold leading-normal w-[399px] h-[74px] mb-[24px] pr-[100px] '>Different writing <span className='bg-clip-text bg-gradient-to-r from-[#4EA0FB] via-[#9354F7] to-[#D70BF4] text-transparent'>Styles</span> for better output</p></div>
+      <div ><p className='text-white text-opacity-50 font-poppins text-sm font-normal leading-normal w-[314px] h-[72px] pr-[24px]'>A powerhouse web application empowering users to excel in writing, learning, and presenting.</p></div>
+      <div className="flex items-center mt-[32px]">
+        <Link className='mr-2' href="/">
+          <button className="w-[143px] h-[44px] flex-shrink-0 rounded-[22px] bg-[rgba(255,255,255,0.2)]"><p className='text-white text-center font-montserrat text-base font-medium leading-normal'>Try For Free</p></button>
+        </Link>
+        <Link className='ml-2' href="/">
+          <button className="w-[130px] h-[44px] flex-shrink-0 rounded-[22px] border border-[rgba(255,255,255,0.7)]"><p className='text-white text-center font-montserrat text-base font-medium leading-normal'>Learn More</p></button>
+        </Link>
+      </div>
+      </div>
+      </div>
+      <div className=''>
+      <Image className='' src={learn} alt='Learn' />
+      </div>
+    </div>
     </>
   )
 
   const learnContent = (
     <>
-      <p>This is the learn content.</p>
-      <Image src='/learn.png' alt='Learn' />
+    <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center'>
+      <div className='w-[399px] h-[364px]  '>
+      <div ><p className='text-white font-poppins text-base font-medium leading-normal mb-[26px]'>Simple Uni</p></div>
+      <div ><p className='text-white font-montserrat text-2xl font-bold leading-normal w-[399px] h-[74px] mb-[38px] pr-[100px] '>Summarized and Explained with <span className='bg-clip-text bg-gradient-to-r from-[#4EA0FB] via-[#9354F7] to-[#D70BF4] text-transparent'>Learn</span> feature</p></div>
+      <div ><p className='text-white text-opacity-50 font-poppins text-sm font-normal leading-normal w-[314px] h-[72px] pr-[24px]'>A powerhouse web application empowering users to excel in writing, learning, and presenting.</p></div>
+      <div className="flex items-center mt-[32px]">
+        <Link className='mr-2' href="/">
+          <button className="w-[143px] h-[44px] flex-shrink-0 rounded-[22px] bg-[rgba(255,255,255,0.2)]"><p className='text-white text-center font-montserrat text-base font-medium leading-normal'>Try For Free</p></button>
+        </Link>
+        <Link className='ml-2' href="/">
+          <button className="w-[130px] h-[44px] flex-shrink-0 rounded-[22px] border border-[rgba(255,255,255,0.7)]"><p className='text-white text-center font-montserrat text-base font-medium leading-normal'>Learn More</p></button>
+        </Link>
+      </div>
+      </div>
+      </div>
+      <div className=''>
+      <Image className='' src={learn} alt='Learn' />
+      </div>
+    </div>
     </>
   )
 
   const presentContent = (
     <>
-      <p>This is the present content.</p>
-      <Image src='/present.png' alt='Present' />
+    <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center'>
+      <div className='w-[399px] h-[364px]  '>
+      <div ><p className='text-white font-poppins text-base font-medium leading-normal mb-[26px]'>Simple Uni</p></div>
+      <div ><p className='text-white font-montserrat text-2xl font-bold leading-normal w-[399px] h-[74px] mb-[38px] pr-[100px] '>Generate quick <span className='bg-clip-text bg-gradient-to-r from-[#4EA0FB] via-[#9354F7] to-[#D70BF4] text-transparent'>Presentation</span> from pointers</p></div>
+      <div ><p className='text-white text-opacity-50 font-poppins text-sm font-normal leading-normal w-[314px] h-[72px] pr-[24px]'>A powerhouse web application empowering users to excel in writing, learning, and presenting.</p></div>
+      <div className="flex items-center mt-[32px]">
+        <Link className='mr-2' href="/">
+          <button className="w-[143px] h-[44px] flex-shrink-0 rounded-[22px] bg-[rgba(255,255,255,0.2)]"><p className='text-white text-center font-montserrat text-base font-medium leading-normal'>Try For Free</p></button>
+        </Link>
+        <Link className='ml-2' href="/">
+          <button className="w-[130px] h-[44px] flex-shrink-0 rounded-[22px] border border-[rgba(255,255,255,0.7)]"><p className='text-white text-center font-montserrat text-base font-medium leading-normal'>Learn More</p></button>
+        </Link>
+      </div>
+      </div>
+      </div>
+      <div className=''>
+      <Image className='' src={learn} alt='Learn' />
+      </div>
+    </div>
     </>
   )
 
   return (
-    <div>
-      <div className='flex justify-end'>
-        <button onClick={() => setSelected('write')}>Write</button>
-        <button onClick={() => setSelected('learn')}>Learn</button>
-        <button onClick={() => setSelected('present')}>Present</button>
-      </div>
-      <div className='flex'>
-        <div className='w-1/2'>
-          {selected === 'write' && writeContent}
-          {selected === 'learn' && learnContent}
-          {selected === 'present' && presentContent}
+    <div className='relative'>
+      <Image className='w-full 1xl:h-[772px] xl:h-[772px]' src={blue} alt='' />
+      <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center flex-col'>
+        <div className='flex justify-center items-center'>
+          <button onClick={() => setSelected('write')} className=' ml-[380px] mr-[14px] w-[110px] h-[29px] flex-shrink-0 rounded-[4px] bg-[rgba(255,255,255,0.2)] '>
+            <p className='text-gray-300 text-opacity-70 font-poppins text-xs font-normal leading-normal'>Simple Write</p>
+          </button>
+          <button onClick={() => setSelected('learn')} className='mr-[14px] w-[110px] h-[29px] flex-shrink-0 rounded-[4px] bg-[rgba(255,255,255,0.2)] '>
+            <p className='text-gray-300 text-opacity-70 font-poppins text-xs font-normal leading-normal'>Simple Learn</p>
+          </button>
+          <button onClick={() => setSelected('present')} className='w-[110px] h-[29px] flex-shrink-0 rounded-[4px] bg-[rgba(255,255,255,0.2)] '>
+            <p className='text-gray-300 text-opacity-70 font-poppins text-xs font-normal leading-normal'>Simple Present</p>
+          </button>
         </div>
-        <div className='w-1/2'>
-          {/* Image related to the selected content */}
+        <div className=''>
+          <div className=''>
+            {selected === 'write' && writeContent}
+            {selected === 'learn' && learnContent}
+            {selected === 'present' && presentContent}
+          </div>
         </div>
       </div>
     </div>
   )
+  
 }
