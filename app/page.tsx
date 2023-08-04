36e58637ff016fc4441a6d770283ba7b07d5facc
nadrahoda <nadrahoda2412@gmail.com>
2023-08-04 21:15:50 +0530

    mail extension section done

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import Banner from '@/components/Banner'
 import Contact from '../components/Contact'
 import EndlessStory from '@/components/EndlessStory'
+import Simplemail from '@/components/Simplemail'
 
 
 export default function Page() {
   return (
     <>
      {/* <Hero/>  */}
+     <Simplemail />
      <EndlessStory/>
     <Banner/> 
     <Contact/>
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 const Banner = () => {
   return (
     <section className="">
-      <div className="w-full h-0 relative " style={{ paddingBottom: "35%" }}>
+      <div className="banner w-full h-0 relative " style={{ paddingBottom: "35%" }}>
         <Image
           src="/images/banner.svg"
           alt="Banner Image"
           layout="fill"
           objectFit="cover"
+          className="bimage"
         />
       </div>
     </section>
