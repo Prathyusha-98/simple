e50310db605a55bb8554242481367f83dfb2f280
Prathyusha <prathyusha@simplemail.ai>
2023-08-07 16:40:36 +0530

    simplemai

--- components/Simplemail.tsx
+++ components/Simplemail.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import Image from "next/image";
 import mailpic from "@/assets/mailpic.svg"
@@ -43,51 +43,4 @@ const Simplemail = () => {
 };
 
 export default Simplemail;
-=======
-import React from "react";
-import Image from "next/image";
-import MailBg from "../assets/mailbg.svg"
-import Mail from "../assets/mailpic.svg"
 
-
-const Simplemail = () => {
-  return (
-    <section className="relative mailsection">
-      <div
-        className="mail w-full h-0 relative "
-        style={{ paddingBottom: "30%" }}
-      >
-        <Image
-          src={ MailBg}
-          alt="Mail Image"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-
-      <div className="mailtext absolute top-0 left-0 w-full h-full flex flex-col items-centerjustify-center">
-        <h2 className="text-white text-center">
-          Also try our <span>Simplemail</span> extension for Gmail
-        </h2>
-        <h4 className="mailpara text-center">
-          Quick react, AI reply, AI compose mail and many more features.
-        </h4>
-        <div className="mbutton">
-          {" "}
-          <button className="mailbutton">Try For Free</button>
-        </div>
-        <div className="mailimage">
-          <Image
-            src={Mail}
-            alt="Mail Image"
-            height={342}
-            width={991}
-          />
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default Simplemail;
->>>>>>> ea1252d0e57f978a6b7986723e3ea605ed577bf8
