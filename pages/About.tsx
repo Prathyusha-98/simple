bb069b94e6e0b0d816283ebce18a24c9ba61e5bd
Prathyusha <prathyusha@simplemail.ai>
2023-09-14 15:02:43 +0530

    about

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,28 +1,5 @@
 "use client"
-import React, { useState, useEffect, useRef  } from 'react';
-import Image from 'next/image';
-import Header from '@/components/Header';
-import Contact from '@/components/Contact';
-import kin from '@/assets/kin.png';
-import hena from '@/assets/hena.png';
-import praveen from '@/assets/praveen.png';
-import amarjeet from '@/assets/amarjeet.png';
-import karun from '@/assets/karun.png';
-import akshay from '@/assets/akshay.png';
-import vikrant from '@/assets/vikrant.png';
-import prathyu from '@/assets/prathyu.png';
-import sohail from '@/assets/sohail.png';
-import manu from '@/assets/manu.png';
-import prabakar from '@/assets/prabakar.png';
-import sonu from '@/assets/sonu.png'
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import Footer from '@/components/Footer';
-import Link from 'next/link';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import logo from '@/assets/logo.png';
-import { useRouter } from 'next/router';
-import HeaderMobile from '@/components/HeaderMobile';
+import React from 'react';
 import About from '@/components/About';
 
 
